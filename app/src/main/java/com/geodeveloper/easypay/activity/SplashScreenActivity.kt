15df6862eb7731bc1292a4818f7dbbaaa6717887
Geotechbrain <ejilolahammed2000@gmail.com>
2020-10-08 13:54:34 -0700

    First release

---
+++ app/src/main/java/com/geodeveloper/easypay/activity/SplashScreenActivity.kt
@@ -0,0 +1,27 @@
+package com.geodeveloper.easypay.activity
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import com.blogspot.atifsoftwares.animatoolib.Animatoo
+import com.geodeveloper.easypay.R
+
+class SplashScreenActivity : AppCompatActivity() {
+    private var mDelayHandler: Handler? = null
+    private val SPLASH_DELAY: Long = 5000
+    private val mRunnable: Runnable = Runnable {
+        if (!isFinishing) {
+            startActivity(Intent(applicationContext, OnBoardActivity::class.java))
+            Animatoo.animateFade(this)
+            finish()
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+        mDelayHandler = Handler()
+        mDelayHandler!!.postDelayed(mRunnable, SPLASH_DELAY)
+    }
+}

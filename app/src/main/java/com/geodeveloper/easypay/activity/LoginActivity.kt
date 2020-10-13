efcd12efc537f53edcfc27fcd7c32cc206e9e569
EngrAdam <engr.adam77@gmail.com>
2020-10-13 11:57:16 +0100

    frontend initial commit

--- app/src/main/java/com/geodeveloper/easypay/activity/LoginActivity.kt
+++ app/src/main/java/com/geodeveloper/easypay/activity/LoginActivity.kt
@@ -2,6 +2,7 @@ package com.geodeveloper.easypay.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.geodeveloper.easypay.R
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {

980541e75d92ae670d75cc2711c07fb938156f1d
Sambo Hassan <sambohasan@gmail.com>
2020-10-17 07:54:23 +0100

    UPDATE: updated the frontend and added new UI features

--- app/src/main/java/com/geodeveloper/easypay/activity/MainActivity.kt
+++ app/src/main/java/com/geodeveloper/easypay/activity/MainActivity.kt
@@ -2,11 +2,19 @@ package com.geodeveloper.easypay.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.ActionBar
 import com.geodeveloper.easypay.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+ /*   lateinit var toolbar: ActionBar*/
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        /*toolbar = supportActionBar!!
+        val bottomNavigation: BottomNavigationView = findViewById(R.id.bottom_navigation)*/
     }
 }
---
+++ app/src/main/java/com/geodeveloper/easypay/activity/PhoneVerificationActivity.kt
@@ -0,0 +1,12 @@
+package com.geodeveloper.easypay.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.geodeveloper.easypay.R
+
+class PhoneVerificationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_phone_verification)
+    }
+}

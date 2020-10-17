6b553a0a6b9890344dcfe9dec232613f0259d1cd
Sambo Hassan <sambohasan@gmail.com>
2020-10-17 14:52:28 +0100

    [UPDATE]: added ResetPassword activity xml Layout and contact fragment

--- app/src/main/java/com/geodeveloper/easypay/activity/ResetPasswordActivity.kt
+++ app/src/main/java/com/geodeveloper/easypay/activity/ResetPasswordActivity.kt
@@ -2,18 +2,11 @@ package com.geodeveloper.easypay.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.appcompat.app.ActionBar
 import com.geodeveloper.easypay.R
-import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class MainActivity : AppCompatActivity() {
-
-
-
+class ResetPasswordActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-
+        setContentView(R.layout.activity_reset_password)
     }
 }
